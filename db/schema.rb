290ec9a7509e05a97c538ac02167e6f589bef240
Kabaddi4 <banana02kare@gmail.com>
2022-02-16 10:50:08 +0000

    [create]user#index,show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_085256) do
+ActiveRecord::Schema.define(version: 2022_02_16_103405) do
+
+  create_table "targets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
