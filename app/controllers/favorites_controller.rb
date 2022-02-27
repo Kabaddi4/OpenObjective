26f049b6a9f5051689186087d6ebad5461ee2ff4
Kabaddi4 <banana02kare@gmail.com>
2022-02-27 08:28:09 +0000

    [create]favorite ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
+    @target = Target.find(params[:target_id])
     target = Target.find(params[:target_id])
     favorite = current_user.favorites.new(target_id: target.id)
     favorite.save
-    #非同期実装の際消去
-
   end
 
   def destroy
+    @target = Target.find(params[:target_id])
     target = Target.find(params[:target_id])
     favorite = current_user.favorites.find_by(target_id: target.id)
     favorite.destroy
-    #非同期実装の際消去
-
   end
 end
